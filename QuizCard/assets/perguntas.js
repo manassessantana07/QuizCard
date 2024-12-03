d1d90dee7d299b035dfa17b377145ed9db2cec85
manassessantana07 <manassesjlsantana@gmail.com>
2024-12-03 19:07:35 -0300

    QuizCard

---
+++ QuizCard/assets/perguntas.js
@@ -0,0 +1,107 @@
+criaCartao(
+    'Matemática',
+    'Qual é a raiz cúbica de 5?',
+    '5³ é igual á 5 x 5 x 5, então 125'
+);
+
+criaCartao(
+    'Literatura',
+    'Três fases da Literatura Brasileira',
+    'Romantismo, Realismo e Simbolismo'
+);
+
+criaCartao(
+   'Lingua Inglesa',
+    'O que significa too?',
+    'Significa Também'
+);
+
+criaCartao(
+    'Geografia',
+     'Qual é a linha imaginária que divide em dois lados a Terra?',
+     'A linha se chama "Meridiano de Greenwich"'
+ );
+
+ criaCartao(
+    'Matemática',
+     'Quanto é 8x8?',
+     '8x8 é igual á 64'
+ );
+
+ criaCartao(
+    'Geografia',
+     'Trópicos do Globo Terrestre',
+     'Trópico de Câncer e Trópico de Capricórnio'
+ );
+
+ criaCartao(
+    'Lingua Portuguesa',
+     'Qual é a diferença entre tem e têm?',
+     '"Tem" sem circunflexo é singular e "têm" com circunflexo é plural'
+ );
+
+ criaCartao(
+    'Lingua Inglesa',
+     'Traduza: Yes, I am from Brazilia',
+     'Sim, eu sou de Brasília'
+ );
+
+ criaCartao(
+    'Geografia',
+     'Maior oceano do Planeta',
+     'Oceano Pacífico'
+ );
+
+ criaCartao(
+    'Matemática',
+     'Qual é o valor de π até a terceira casa decimal?',
+     '3,141'
+ );
+
+ criaCartao(
+    'Geografia',
+     'Qual é o maior país do mundo em área territorial?',
+     'Rússia'
+ );
+
+ criaCartao(
+    'História',
+     'Quem foi o imperador romano responsável pela construção do Coliseu?',
+     'Imperador Vespasiano'
+ );
+
+ criaCartao(
+    'Ciências',
+     'Qual é o nome do processo pela qual as plantas produzem seu próprio alimento?',
+     'Fotossíntese'
+ );
+
+ criaCartao(
+    'Literatura',
+     'Quem escreveu o livro "Dom Quixote"?',
+     'Miguel de Cervantes'
+ );
+
+ criaCartao(
+    'Física',
+     'Qual é a unidade de medida da força no Sistema Internacional de Unidades?',
+     'Newton (N)'
+ );
+
+ criaCartao(
+    'Química',
+    'O que é a tabela periódica e quem foi o seu criador?',
+    'A tabela periódica organiza os elementos químicos de acordo com suas propriedades. Seu criador foi Dmitri Mendeliev'
+ );
+
+ criaCartao(
+    'Arte',
+     'Quem pintou o famoso quadro "Mona Lisa"?',
+     'Leonardo da Vinci'
+ );
+
+ criaCartao(
+    'Educação Física',
+     'Qual é o nome do esporte que mistura futebol e vôlei, sendo jogado com a mão?',
+     'É o Futsal'
+ );
